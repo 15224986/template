3087c0555bf27911dadac0ae495499723488d88a
15224986 <35017110+15224986@users.noreply.github.com>
2019-09-25 09:24:23 +0800

    Add files via upload

---
+++ vendor/echarts/map/geoCoord.js
@@ -0,0 +1,138 @@
+var allProviance = [
+	{
+		cityname: "北京",
+		itemvalue: undefined
+	},
+	{
+		cityname: "天津",
+		itemvalue: undefined
+	},
+	{
+		cityname: "上海",
+		itemvalue: undefined
+	},
+	{
+		cityname: "重庆",
+		itemvalue: undefined
+	},
+	{
+		cityname: "河北",
+		itemvalue: undefined
+	},
+	{
+		cityname: "山西",
+		itemvalue: undefined
+	},
+	{
+		cityname: "辽宁",
+		itemvalue: undefined
+	},
+	{
+		cityname: "吉林",
+		itemvalue: undefined
+	},
+	{
+		cityname: "黑龙江",
+		itemvalue: undefined
+	}, 
+	{
+		cityname: "江苏",
+		itemvalue: undefined
+	},
+	{
+		cityname: "浙江",
+		itemvalue: undefined
+	},
+	{
+		cityname: "安徽",
+		itemvalue: undefined
+	},
+	{
+		cityname: "福建",
+		itemvalue: undefined
+	},
+	{
+		cityname: "江西",
+		itemvalue: undefined
+	},
+	{
+		cityname: "山东",
+		itemvalue: undefined
+	},
+	{
+
+		cityname: "河南",
+		itemvalue: undefined
+	},
+	{
+		cityname: "湖北",
+		itemvalue: undefined
+	},
+	{
+		cityname: "广东",
+		itemvalue: undefined
+	},
+	{
+		cityname: "海南",
+		itemvalue: undefined
+	},
+	{
+		cityname: "四川",
+		itemvalue: undefined
+	},
+	{
+		cityname: "贵州",
+		itemvalue: undefined
+	},
+	{
+		cityname: "云南",
+		itemvalue: undefined
+	},
+	{
+
+		cityname: "陕西",
+		itemvalue: undefined
+	},
+	{
+		cityname: "甘肃",
+		itemvalue: undefined
+	},
+	{
+		cityname: "青海",
+		itemvalue: undefined
+	},
+	{
+		cityname: "台湾",
+		itemvalue: undefined
+	},
+	{
+
+		cityname: "内蒙古",
+		itemvalue: undefined
+	},
+	{
+		cityname: "广西",
+		itemvalue: undefined
+	},
+	{
+		cityname: "西藏",
+		itemvalue: undefined
+	},
+	{
+		cityname: "宁夏",
+		itemvalue: undefined
+	},
+	{
+		cityname: "新疆",
+		itemvalue: undefined
+	},
+	{
+		cityname: "香港",
+		itemvalue: undefined
+	},
+	{
+		cityname: "澳门",
+		itemvalue: undefined
+	}
+];
+
